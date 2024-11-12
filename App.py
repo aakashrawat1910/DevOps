29e36cb1db713a10cea42ffd369459951025780b
Aakash Rawat <aakashrawat1910@gmail.com>
2024-11-12 15:22:18 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, jsonify
 
 appname = Flask(__name__)
-B=2
+
 @appname.route("/")
-#this is master change
-#this is change in bigfix
-#Master 2
 
 
 def hello():
     return "hello world"
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
 
 
 
